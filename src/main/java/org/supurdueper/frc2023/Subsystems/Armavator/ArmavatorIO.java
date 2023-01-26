2ef35255d923b3731c1212aa6c141f19891eb3cf
perc3va1 <noah.smith@pphs.purdue.edu>
2023-01-26 09:32:33 -0500

    started constructing ArmavatorIO

--- src/main/java/org/supurdueper/frc2023/Subsystems/Armavator/ArmavatorIO.java
+++ src/main/java/org/supurdueper/frc2023/Subsystems/Armavator/ArmavatorIO.java
@@ -6,6 +6,21 @@ public interface ArmavatorIO {
     @AutoLog
     public static class ArmavatorIOInputs {
         public double armPosition = 0.0;
+        public double armVelocity = 0.0;
+        public double armAppliedVolts = 0.0;
+        public double[] arm1CurrentAmps = new double[] {};
+        public double[] arm2CurrentAmps = new double[] {};
+        public double[] arm1Temp = new double[] {};
+        public double[] arm2Temp = new double[] {};
+
+        public double elevatorPosition = 0.0;
+        public double elevatorVelocity = 0.0;
+        public double elevatorAppliedVolts = 0.0;
+        public double[] elevatorCurrentAmps = new double[] {};
+        public double[] elevatorTemp = new double[] {};
     }
-    
+    /* Updates the sets of loggable inputs */
+    public default void updateArmavatorInputs(ArmavatorIOInputs Inputs) {}
+    /* Run the arm motor at a specifc Voltage */
+    public default void setArmVoltage(double volts) {}
 }
