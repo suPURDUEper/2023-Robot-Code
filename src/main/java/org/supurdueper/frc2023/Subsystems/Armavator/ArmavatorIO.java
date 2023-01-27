88240c47de53a98d90888fe217ddf81384597bc8
perc3va1 <noah.smith@pphs.purdue.edu>
2023-01-26 19:39:22 -0500

    added armavatarIOSparkMax file

--- src/main/java/org/supurdueper/frc2023/Subsystems/Armavator/ArmavatorIO.java
+++ src/main/java/org/supurdueper/frc2023/Subsystems/Armavator/ArmavatorIO.java
@@ -3,24 +3,28 @@ package org.supurdueper.frc2023.subsystems.Armavator;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface ArmavatorIO {
-    @AutoLog
-    public static class ArmavatorIOInputs {
-        public double armPosition = 0.0;
-        public double armVelocity = 0.0;
-        public double armAppliedVolts = 0.0;
-        public double[] arm1CurrentAmps = new double[] {};
-        public double[] arm2CurrentAmps = new double[] {};
-        public double[] arm1Temp = new double[] {};
-        public double[] arm2Temp = new double[] {};
+  @AutoLog
+  public static class ArmavatorIOInputs {
+    public double armPosition = 0.0;
+    public double armVelocity = 0.0;
+    public double armAppliedVolts = 0.0;
+    public double[] armCurrentAmps = new double[] {};
+    public double[] armTemp = new double[] {};
 
-        public double elevatorPosition = 0.0;
-        public double elevatorVelocity = 0.0;
-        public double elevatorAppliedVolts = 0.0;
-        public double[] elevatorCurrentAmps = new double[] {};
-        public double[] elevatorTemp = new double[] {};
-    }
-    /* Updates the sets of loggable inputs */
-    public default void updateArmavatorInputs(ArmavatorIOInputs Inputs) {}
-    /* Run the arm motor at a specifc Voltage */
-    public default void setArmVoltage(double volts) {}
+    public double elevatorPosition = 0.0;
+    public double elevatorVelocity = 0.0;
+    public double elevatorAppliedVolts = 0.0;
+    public double[] elevatorCurrentAmps = new double[] {};
+    public double[] elevatorTemp = new double[] {};
+  }
+  /* Updates the sets of loggable inputs */
+  public default void updateArmavatorInputs(ArmavatorIOInputs Inputs) {}
+  /* Run the arm motor at a specifc Voltage */
+  public default void setArmVoltage(double volts) {}
+  /* Run the elevator motor at a specifc Voltage */
+  public default void setElevatorVoltage(double volts) {}
+  /* enable/disable brake mode for arm motor */
+  public default void setArmBrakeMode(boolean enable) {}
+  /* enable/disable brake mode for elevator motor */
+  public default void setElevatorBrakeMode(boolean enable) {}
 }
---
+++ src/main/java/org/supurdueper/frc2023/Subsystems/Armavator/ArmavatorIOSparkMax.java
@@ -0,0 +1,39 @@
+package org.supurdueper.frc2023.subsystems.Armavator;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.wpilibj.RobotController;
+
+public class ArmavatorIOSparkMax implements ArmavatorIO {
+  private final CANSparkMax armSparkMax;
+  private final CANSparkMax elevatorSparkMax;
+
+  private final RelativeEncoder armEncoder;
+  private final RelativeEncoder elevatorEncoder;
+
+  public ArmavatorIOSparkMax() {
+    elevatorSparkMax = new CANSparkMax(9, MotorType.kBrushless);
+    armSparkMax = new CANSparkMax(10, MotorType.kBrushless);
+
+    armEncoder = armSparkMax.getEncoder();
+    armEncoder.setPosition(0);
+    elevatorEncoder = elevatorSparkMax.getEncoder();
+    elevatorEncoder.setPosition(0);
+  }
+
+  public void updateInputs(ArmavatorIOInputs inputs) {
+    inputs.armPosition = armSparkMax.getEncoder().getPosition();
+    inputs.elevatorPosition = elevatorSparkMax.getEncoder().getPosition();
+
+    inputs.armAppliedVolts = armSparkMax.getAppliedOutput() * RobotController.getBatteryVoltage();
+    inputs.elevatorAppliedVolts =
+        elevatorSparkMax.getAppliedOutput() * RobotController.getBatteryVoltage();
+
+    inputs.armCurrentAmps = new double[] {armSparkMax.getOutputCurrent()};
+    inputs.elevatorCurrentAmps = new double[] {elevatorSparkMax.getOutputCurrent()};
+
+    inputs.armTemp = new double[] {armSparkMax.getMotorTemperature()};
+    inputs.elevatorTemp = new double[] {elevatorSparkMax.getMotorTemperature()};
+  }
+}
