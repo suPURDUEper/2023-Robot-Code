10bc4c234515831454403695cd32205ebf5559bd
perc3va1 <noah.smith@pphs.purdue.edu>
2023-01-28 11:25:33 -0500

    created ArmavatorMotor.java

---
+++ src/main/java/org/supurdueper/frc2023/Subsystems/Armavator/ArmavatorMotor.java
@@ -0,0 +1,9 @@
+package org.supurdueper.frc2023.subsystems.Armavator;
+
+public class ArmavatorMotor {
+    private final ArmavatorMotorIO io;
+    
+    public ArmavatorMotor(ArmavatorMotorIO io) {
+        this.io = io;
+    }
+}
