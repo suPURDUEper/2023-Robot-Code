50ea9139ebae6f62024f9d9c605b410a592145fd
Rahul Yarlagadda <knufire@gmail.com>
2023-01-31 17:16:07 -0500

    Spotless Apply

--- src/main/java/org/supurdueper/frc2023/Subsystems/Armavator/ArmavatorMotor.java
+++ src/main/java/org/supurdueper/frc2023/Subsystems/Armavator/ArmavatorMotor.java
@@ -1,90 +1,87 @@
 package org.supurdueper.frc2023.subsystems.Armavator;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import org.littletonrobotics.frc2023.Constants;
 import org.littletonrobotics.frc2023.util.LoggedTunableNumber;
 import org.littletonrobotics.junction.Logger;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-
 public class ArmavatorMotor {
-    private final ArmavatorMotorIO io;
-    private final int index;
-    private final ArmavatorMotorIOInputsAutoLogged inputs = new ArmavatorMotorIOInputsAutoLogged();
+  private final ArmavatorMotorIO io;
+  private final int index;
+  private final ArmavatorMotorIOInputsAutoLogged inputs = new ArmavatorMotorIOInputsAutoLogged();
 
-    private final double sprocketPitch = 1.75;
-    private static final LoggedTunableNumber armKp = 
-    new LoggedTunableNumber("Arm/Motor/Kp");
-    private static final LoggedTunableNumber armKd = 
-    new LoggedTunableNumber("Arm/Motor/Kd");
-    private static final LoggedTunableNumber armKs =
-    new LoggedTunableNumber("Arm/Motor/Ks");
-    private static final LoggedTunableNumber armKv = 
-    new LoggedTunableNumber("Arm/Motor/Kv");
-    private static final LoggedTunableNumber elevatorKp = 
-    new LoggedTunableNumber("Elevator/Motor/Kp");
-    private static final LoggedTunableNumber elevatorKd = 
-    new LoggedTunableNumber("Elevator/Motor/Kd");
-    private static final LoggedTunableNumber elevatorKs =
-    new LoggedTunableNumber("Elevator/Motor/Ks");
-    private static final LoggedTunableNumber elevatorKv = 
-    new LoggedTunableNumber("Elevator/Motor/Kv");
+  private final double sprocketPitch = 1.75;
+  private static final LoggedTunableNumber armKp = new LoggedTunableNumber("Arm/Motor/Kp");
+  private static final LoggedTunableNumber armKd = new LoggedTunableNumber("Arm/Motor/Kd");
+  private static final LoggedTunableNumber armKs = new LoggedTunableNumber("Arm/Motor/Ks");
+  private static final LoggedTunableNumber armKv = new LoggedTunableNumber("Arm/Motor/Kv");
+  private static final LoggedTunableNumber elevatorKp =
+      new LoggedTunableNumber("Elevator/Motor/Kp");
+  private static final LoggedTunableNumber elevatorKd =
+      new LoggedTunableNumber("Elevator/Motor/Kd");
+  private static final LoggedTunableNumber elevatorKs =
+      new LoggedTunableNumber("Elevator/Motor/Ks");
+  private static final LoggedTunableNumber elevatorKv =
+      new LoggedTunableNumber("Elevator/Motor/Kv");
 
-    private SimpleMotorFeedforward armFeedforward = new SimpleMotorFeedforward(0.0, 0.0);
-    private SimpleMotorFeedforward elevatorFeedforward = new SimpleMotorFeedforward(0.0, 0.0);
-    private final PIDController armFeedback = 
-    new PIDController(0.0, 0.0, 0.0, Constants.loopPeriodSecs);
-    private final PIDController elevatorFeedback =
-    new PIDController(0, 0, 0, Constants.loopPeriodSecs);
+  private SimpleMotorFeedforward armFeedforward = new SimpleMotorFeedforward(0.0, 0.0);
+  private SimpleMotorFeedforward elevatorFeedforward = new SimpleMotorFeedforward(0.0, 0.0);
+  private final PIDController armFeedback =
+      new PIDController(0.0, 0.0, 0.0, Constants.loopPeriodSecs);
+  private final PIDController elevatorFeedback =
+      new PIDController(0, 0, 0, Constants.loopPeriodSecs);
 
-    static {
-        armKp.initDefault(0.1);
-        armKd.initDefault(0.0);
-        armKs.initDefault(0.12349);
-        armKv.initDefault(0.13477);
-        elevatorKp.initDefault(0.1);
-        elevatorKd.initDefault(0.0);
-        elevatorKs.initDefault(0.12349);
-        elevatorKv.initDefault(0.13477);
-    }
-    
-    public ArmavatorMotor(ArmavatorMotorIO io, int index) {
-        this.io = io;
-        this.index = index;
-    }
-    public void periodic() {
-        io.updateArmavatorInputs(inputs);
-        Logger.getInstance().processInputs("Armavator/Motors" + Integer.toString(index), inputs);
+  static {
+    armKp.initDefault(0.1);
+    armKd.initDefault(0.0);
+    armKs.initDefault(0.12349);
+    armKv.initDefault(0.13477);
+    elevatorKp.initDefault(0.1);
+    elevatorKd.initDefault(0.0);
+    elevatorKs.initDefault(0.12349);
+    elevatorKv.initDefault(0.13477);
+  }
 
-        // Update controllers if tunable numbers have changed
-        if (armKp.hasChanged(hashCode()) || armKd.hasChanged(hashCode())) {
-            armFeedback.setPID(armKp.get(), 0.0, armKd.get());
-        }
-        if (elevatorKp.hasChanged(hashCode()) || elevatorKd.hasChanged(hashCode())) {
-            elevatorFeedback.setPID(elevatorKp.get(), 0.0, elevatorKd.get());
-        }
-        if (armKs.hasChanged(hashCode()) || armKv.hasChanged(hashCode())) {
-            armFeedforward = new SimpleMotorFeedforward(armKs.get(), armKv.get());
-        }
-        if (elevatorKs.hasChanged(hashCode()) || elevatorKv.hasChanged(hashCode())) {
-            elevatorFeedforward = new SimpleMotorFeedforward(elevatorKs.get(), elevatorKv.get());
-        }
-    }
-    public void Stop() {
-        io.setArmVoltage(0);
-        io.setElevatorVoltage(0);
-    }
+  public ArmavatorMotor(ArmavatorMotorIO io, int index) {
+    this.io = io;
+    this.index = index;
+  }
 
-    public void setBrakeMode(boolean enabled) {
-        io.setArmBrakeMode(enabled);
-        io.setElevatorBrakeMode(enabled);
-    }
+  public void periodic() {
+    io.updateArmavatorInputs(inputs);
+    Logger.getInstance().processInputs("Armavator/Motors" + Integer.toString(index), inputs);
 
-    public double getArmVelocity() {
-        return inputs.armVelocity;
+    // Update controllers if tunable numbers have changed
+    if (armKp.hasChanged(hashCode()) || armKd.hasChanged(hashCode())) {
+      armFeedback.setPID(armKp.get(), 0.0, armKd.get());
     }
+    if (elevatorKp.hasChanged(hashCode()) || elevatorKd.hasChanged(hashCode())) {
+      elevatorFeedback.setPID(elevatorKp.get(), 0.0, elevatorKd.get());
+    }
+    if (armKs.hasChanged(hashCode()) || armKv.hasChanged(hashCode())) {
+      armFeedforward = new SimpleMotorFeedforward(armKs.get(), armKv.get());
+    }
+    if (elevatorKs.hasChanged(hashCode()) || elevatorKv.hasChanged(hashCode())) {
+      elevatorFeedforward = new SimpleMotorFeedforward(elevatorKs.get(), elevatorKv.get());
+    }
+  }
 
-    public double getElevatorVelocity() {
-        return inputs.elevatorVelocity * sprocketPitch; 
-    }
+  public void Stop() {
+    io.setArmVoltage(0);
+    io.setElevatorVoltage(0);
+  }
+
+  public void setBrakeMode(boolean enabled) {
+    io.setArmBrakeMode(enabled);
+    io.setElevatorBrakeMode(enabled);
+  }
+
+  public double getArmVelocity() {
+    return inputs.armVelocity;
+  }
+
+  public double getElevatorVelocity() {
+    return inputs.elevatorVelocity * sprocketPitch;
+  }
 }
