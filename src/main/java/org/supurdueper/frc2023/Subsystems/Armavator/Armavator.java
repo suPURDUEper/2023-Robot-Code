a315406f106f891228f6a2a84bd9ec7d9e3ed23b
perc3va1 <noah.smith@pphs.purdue.edu>
2023-02-02 19:55:32 -0500

    added enum for armavator positions

--- src/main/java/org/supurdueper/frc2023/Subsystems/Armavator/Armavator.java
+++ src/main/java/org/supurdueper/frc2023/Subsystems/Armavator/Armavator.java
@@ -4,6 +4,7 @@
 
 package org.supurdueper.frc2023.subsystems.armavator;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Armavator extends SubsystemBase {
@@ -17,6 +18,34 @@ public class Armavator extends SubsystemBase {
     
     armavatorMotor.setBrakeMode(isBrakeMode);
   }
+  public static record ArmavatorPose(Rotation2d armAngle, double elevatorDistance) {
+    public static enum armavatorPreset {
+      stowed(new ArmavatorPose(new Rotation2d(Math.PI/4), 0.0)),
+      intake(new ArmavatorPose(new Rotation2d(Math.PI/4), 0.5)),
+      low(new ArmavatorPose(new Rotation2d(Math.PI/4), 1.0)),
+      midCone(new ArmavatorPose(new Rotation2d(Math.PI/4), 0.5)),
+      midCube(new ArmavatorPose(new Rotation2d(Math.PI/4), 0.5)),
+      highCone(new ArmavatorPose(new Rotation2d(Math.PI/4), 0.5)),
+      highCube(new ArmavatorPose(new Rotation2d(Math.PI/4), 0.5));
+
+
+      private ArmavatorPose pose;
+
+    private armavatorPreset(ArmavatorPose pose) {
+      this.pose = pose;
+    }
+
+    private void setPreset(ArmavatorPose pose) {
+      this.pose = pose;
+    }
+
+    public ArmavatorPose getPose() {
+      return pose;
+    }
+    }
+   }
+  
+
 
   @Override
   public void periodic() {
