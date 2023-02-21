367ab2468de811c47767d501e5f2dc2e8005ed74
perc3va1 <noah.smith@pphs.purdue.edu>
2023-02-21 18:49:49 -0500

    adjusted elevator setpoints

--- src/main/java/org/supurdueper/frc2023/Subsystems/Armavator.java
+++ src/main/java/org/supurdueper/frc2023/Subsystems/Armavator.java
@@ -6,6 +6,9 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile;
 public interface Armavator {
   public static record ArmavatorPose(
       Rotation2d armAngle, double elevatorDistance, double armVelocity, double elevatorVelocity) {
+        private static final double ELEVATOR_MIN = 0.0;
+        private static final double ELEVATOR_NEUTRAL = 0.2;
+        private static final double ELEVATOR_MAX = 0.6;
 
     public TrapezoidProfile.State getArmProfileState() {
       return new TrapezoidProfile.State(armAngle.getRadians(), armVelocity);
@@ -16,13 +19,13 @@ public interface Armavator {
     }
 
     public static enum ArmavatorPreset {
-      stowed(new ArmavatorPose(new Rotation2d(0), 0.0, 0.0, 0.0)),
-      intake(new ArmavatorPose(new Rotation2d(Math.PI / 4), 0.5, 0.0, 0.0)),
-      low(new ArmavatorPose(new Rotation2d(Math.PI / 4), 1.0, 0.0, 0.0)),
-      midCone(new ArmavatorPose(new Rotation2d(Math.PI / 4), 0.5, 0.0, 0.0)),
-      midCube(new ArmavatorPose(new Rotation2d(Math.PI / 4), 0.5, 0.0, 0.0)),
-      highCone(new ArmavatorPose(new Rotation2d(Math.PI / 4), 0.5, 0.0, 0.0)),
-      highCube(new ArmavatorPose(new Rotation2d(Math.PI / 4), 0.5, 0.0, 0.0)),
+      stowed(new ArmavatorPose(new Rotation2d(-0.4), ELEVATOR_MIN, 0.0, 0.0)),
+      intake(new ArmavatorPose(new Rotation2d(.2), ELEVATOR_NEUTRAL, 0.0, 0.0)),
+      low(new ArmavatorPose(new Rotation2d(.2), ELEVATOR_NEUTRAL, 0.0, 0.0)),
+      midCone(new ArmavatorPose(new Rotation2d(2.1), ELEVATOR_MIN, 0.0, 0.0)),
+      midCube(new ArmavatorPose(new Rotation2d(1.75), ELEVATOR_NEUTRAL, 0.0, 0.0)),
+      highCone(new ArmavatorPose(new Rotation2d(1.78), ELEVATOR_MAX, 0.0, 0.0)),
+      highCube(new ArmavatorPose(new Rotation2d(1.57), ELEVATOR_MAX, 0.0, 0.0)),
       halfway(new ArmavatorPose(new Rotation2d(0), 0.3, 0, 0));
 
       private ArmavatorPose pose;
