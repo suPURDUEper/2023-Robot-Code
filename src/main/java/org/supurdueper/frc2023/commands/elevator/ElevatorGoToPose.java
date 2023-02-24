9a3df8f0aab032f7ef62edd22cae8fa898545fc1
Rahul Yarlagadda <knufire@gmail.com>
2023-02-24 16:26:41 -0500

    Simplify arm and elevator pose commands

--- src/main/java/org/supurdueper/frc2023/commands/elevator/ElevatorGoToPose.java
+++ src/main/java/org/supurdueper/frc2023/commands/elevator/ElevatorGoToPose.java
@@ -5,57 +5,21 @@
 package org.supurdueper.frc2023.commands.elevator;
 
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.TrapezoidProfileCommand;
 import org.supurdueper.frc2023.subsystems.elevator.Elevator;
 
-public class ElevatorGoToPose extends CommandBase {
-
-  private final Elevator elevator;
-  private final TrapezoidProfile.State target;
-  private TrapezoidProfile elevatorProfile;
-  private long startTime;
-  private final TrapezoidProfile.Constraints constraints =
-      new TrapezoidProfile.Constraints(
-          Elevator.elevatorMaxVelocity, Elevator.elevatorMaxAcceleration);
-  ;
-
+public class ElevatorGoToPose extends TrapezoidProfileCommand {
   public ElevatorGoToPose(Elevator elevator, TrapezoidProfile.State target) {
-    this.elevator = elevator;
-    this.target = target;
-    addRequirements(elevator);
-  }
-
-  @Override
-  public void initialize() {
-    elevatorProfile =
+    super(
         new TrapezoidProfile(
-            constraints,
-            target,
-            new TrapezoidProfile.State(
-                elevator.getElevatorPosition(), elevator.getElevatorVelocity()));
-
-    startTime = RobotController.getFPGATime();
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    long elapsedTime = RobotController.getFPGATime() - startTime;
-    double elapsedTimeSeconds = elapsedTime / 1000000.0;
-    TrapezoidProfile.State elevatorState = elevatorProfile.calculate(elapsedTimeSeconds);
-    elevator.setTargetPose(elevatorState.position, elevatorState.velocity);
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    long elapsedTime = RobotController.getFPGATime() - startTime;
-    double elapsedTimeSeconds = elapsedTime / 1000000.0;
-    return elevatorProfile.isFinished(elapsedTimeSeconds);
+            // Limit the max acceleration and velocity
+            new TrapezoidProfile.Constraints(
+                Elevator.elevatorMaxVelocity, Elevator.elevatorMaxAcceleration),
+            // End at desired position in meters; implicitly starts at 0
+            target),
+        // Pipe the profile state to the elevator
+        setpointState -> elevator.setTargetPose(setpointState.position, setpointState.velocity),
+        // Require the elevaotr
+        elevator);
   }
 }
