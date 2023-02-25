979d11e1f3dfaab00ef4786393ec6a7a1bc68427
Rahul Yarlagadda <knufire@gmail.com>
2023-02-25 11:14:49 -0500

    Revert "Simplify arm and elevator pose commands"
    This reverts commit 9a3df8f0aab032f7ef62edd22cae8fa898545fc1.

--- src/main/java/org/supurdueper/frc2023/commands/arm/ArmGoToPose.java
+++ src/main/java/org/supurdueper/frc2023/commands/arm/ArmGoToPose.java
@@ -6,22 +6,53 @@ package org.supurdueper.frc2023.commands.arm;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj2.command.TrapezoidProfileCommand;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import org.supurdueper.frc2023.subsystems.arm.Arm;
 
-public class ArmGoToPose extends TrapezoidProfileCommand {
+public class ArmGoToPose extends CommandBase {
+
+  private final Arm arm;
+  private final TrapezoidProfile.State target;
+  private TrapezoidProfile armProfile;
+  private final TrapezoidProfile.Constraints constraints =
+      new TrapezoidProfile.Constraints(Arm.armMaxVelocity, Arm.armMaxAcceleration);
+  private final Timer timer = new Timer();
+  ;
+
   public ArmGoToPose(Arm arm, TrapezoidProfile.State target) {
-    super(
+    this.arm = arm;
+    this.target = target;
+    addRequirements(arm);
+  }
+
+  @Override
+  public void initialize() {
+    armProfile =
         new TrapezoidProfile(
-            // Limit the max acceleration and velocity
-            new TrapezoidProfile.Constraints(Arm.armMaxVelocity, Arm.armMaxAcceleration),
-            // End at desired position in meters; implicitly starts at 0
-            target),
-        // Pipe the profile state to the elevator
-        setpointState ->
-            arm.setTargetPose(
-                Rotation2d.fromRadians(setpointState.position), setpointState.velocity),
-        // Require the elevaotr
-        arm);
+            constraints,
+            target,
+            new TrapezoidProfile.State(arm.getArmPosition().getRadians(), arm.getArmVelocity()));
+    timer.restart();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double elapsedTimeSeconds = timer.get();
+    TrapezoidProfile.State armState = armProfile.calculate(elapsedTimeSeconds);
+    arm.setTargetPose(Rotation2d.fromRadians(armState.position), armState.velocity);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    timer.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return timer.hasElapsed(armProfile.totalTime());
   }
 }
