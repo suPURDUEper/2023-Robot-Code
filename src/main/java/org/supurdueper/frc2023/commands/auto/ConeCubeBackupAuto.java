bc6cc3b80f1aa3a5a4e7395db470726b5c6aab7b
Rahul Yarlagadda <knufire@gmail.com>
2023-03-05 01:42:46 -0500

    Add 2.5ish auto

--- src/main/java/org/supurdueper/frc2023/commands/auto/ConeCubeBackupAuto.java
+++ src/main/java/org/supurdueper/frc2023/commands/auto/ConeCubeBackupAuto.java
@@ -1,9 +1,18 @@
 package org.supurdueper.frc2023.commands.auto;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+import org.littletonrobotics.frc2023.FieldConstants.StagingLocations;
+import org.littletonrobotics.frc2023.commands.DriveToPose;
 import org.littletonrobotics.frc2023.subsystems.drive.Drive;
 import org.littletonrobotics.frc2023.util.AllianceFlipUtil;
+import org.supurdueper.frc2023.commands.arm.ArmGoToPose;
+import org.supurdueper.frc2023.commands.elevator.ElevatorGoToPose;
+import org.supurdueper.frc2023.subsystems.Armavator.ArmavatorPose.ArmavatorPreset;
 import org.supurdueper.frc2023.subsystems.arm.Arm;
 import org.supurdueper.frc2023.subsystems.elevator.Elevator;
 import org.supurdueper.frc2023.subsystems.intake.Intake;
@@ -14,10 +23,18 @@ public class ConeCubeBackupAuto extends SequentialCommandGroup {
    
     Pose2d backup =
         AllianceFlipUtil.apply(
-            new Pose2d()); // TODO
+            new Pose2d(
+                StagingLocations.translations[3],
+                Rotation2d.fromDegrees(-90)));
 
     addCommands(
-        new ConeCubeAuto(drive, elevator, arm, intake)
-    );
+        new ConeCubeAuto(drive, elevator, arm, intake),
+         // Drive to middle of field
+         Commands.parallel(
+            new DriveToPose(drive, backup),
+            Commands.parallel( // Wait so arm doesn't hit grid
+                    new ElevatorGoToPose(elevator, ArmavatorPreset.stowed),
+                    new ArmGoToPose(arm, ArmavatorPreset.stowed))
+                .beforeStarting(Commands.waitSeconds(1))));
   }
 }
